725045792e1c8182ffc92bec9622d6085fcaeb7c
Ellord Tayag <ellord@gmail.com>
2021-09-05 14:43:56 +1000

    Initial PoC using ts-morph to extract envs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,56 @@
 #!/usr/bin/env node
 
-console.log('test');
+import { Project, SyntaxKind } from 'ts-morph';
+
+const tsConfigFilePath = process.argv[2];
+
+const project = new Project({
+  tsConfigFilePath,
+});
+
+const sourceFiles = project.getSourceFiles();
+
+const envs: string[] = [];
+
+sourceFiles.forEach((sourceFile) => {
+  sourceFile.forEachDescendant((node) => {
+    switch (node.getKind()) {
+      // e.g. process.env.HELLO
+      case SyntaxKind.PropertyAccessExpression:
+        if (node.getChildren().length === 3) {
+          node
+            .getChildrenOfKind(SyntaxKind.PropertyAccessExpression)
+            .forEach((children) => {
+              if (children.getText() == 'process.env') {
+                const firstIdentifier = node.getFirstChildByKind(
+                  SyntaxKind.Identifier
+                );
+                if (firstIdentifier) {
+                  envs.push(firstIdentifier.getText());
+                }
+              }
+            });
+        }
+        break;
+
+      // e.g. const { HELLO, WORLD } = process.env
+      case SyntaxKind.ObjectBindingPattern:
+        node.getNextSiblings().forEach((sibling) => {
+          if (sibling.getKind() === SyntaxKind.PropertyAccessExpression) {
+            if (sibling.getText() === 'process.env') {
+              node
+                .getChildrenOfKind(SyntaxKind.BindingElement)
+                .forEach((env) => {
+                  envs.push(env.getText());
+                });
+            }
+          }
+        });
+        break;
+
+      default:
+    }
+  });
+});
+
+console.log('envs', envs);
