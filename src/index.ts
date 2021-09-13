8e61169a86a3aef34f68917e3d414acee747fdea
Ellord Tayag <ellord.tayag@genero.com>
2021-09-13 23:18:44 +1000

    Show currently processing source file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+import * as path from 'path';
+
 import { Project } from 'ts-morph';
 
 import { createExampleEnvFromEnvs } from './createExampleEnvFromEnvs';
@@ -15,6 +17,8 @@ const sourceFiles = project.getSourceFiles();
 
 const envs = new Set<string>();
 sourceFiles.forEach((sourceFile) => {
+  const sourceFilePath = sourceFile.getFilePath();
+  console.log(`Parsing ${path.relative(process.cwd(), sourceFilePath)}`);
   const envsFromSourceFile = extractEnvsFromSourceFile(sourceFile);
   envsFromSourceFile.forEach(envs.add, envs);
 });
